a9831427a2785c4d443282cd4b51674b6396aee9
Evyatar Daud <evyatar.daud@gmail.com>
2020-12-16 12:02:37 +0200

    Move API interaction to PokedexView

--- src/App.js
+++ src/App.js
@@ -1,61 +1,11 @@
-import { useEffect, useState } from 'react';
-import { fetchPokemonData, fetchPokemons } from './api';
-import { Loader } from './components/Loader';
 import { PokedexView } from './components/PokedexView';
-import { DetailsView } from './components/DetailsView';
 import { withRouter } from './HOCs';
+import generations from './generations';
 import './App.css';
 
 function App()
 {
-	const [ pokemons, setPokemons ] = useState( [] );
-	const [ selectedPokemon, setSelectedPokemon ] = useState( -1 );
-	const [ isLoading, setIsLoading ] = useState( false );
 
-
-	// load all pokemons on mount
-	useEffect( () => {
-
-		fillPokemonsArray();
-
-	}, [] );
-
-
-	// fetch all pokemons and push to array
-	const fillPokemonsArray = () => {
-
-		// set loading state and reset pokemons array
-		setIsLoading( true );
-		setPokemons( [] );
-
-		// fetch first original 151 pokemons
-		fetchPokemons( 151 ).then( async ({ results }) => {
-	
-			// iterate over each pokemon an add to array
-			await Promise.all( results.map( async ( pokemon, i ) => {
-
-				await fetchPokemonData( pokemon.name ).then( async ( json ) => {
-
-					await setPokemons( prev => {
-						
-						let tmp = prev.slice();
-						tmp[i] = json;
-						return tmp;
-
-					});
-
-				});
-
-			}));
-
-			setIsLoading( false );
-	
-		});
-		
-	}
-
-
-	// render
 	return (
 		<div className="pokedex-app">
 
@@ -63,14 +13,7 @@ function App()
 				P<i></i>kédex
 			</h1>
 
-			{ selectedPokemon !== -1 &&
-				<DetailsView pokemon={ pokemons[selectedPokemon] } setSelectedPokemon={ setSelectedPokemon } />
-			}
-
-			{ isLoading 
-				? <Loader />
-				: <PokedexView pokemons={ pokemons } setSelectedPokemon={ setSelectedPokemon } />
-			}
+			<PokedexView generation={ generations[0] } />
 
 		</div>
 	);
--- src/components/PokedexView/PokedexView.js
+++ src/components/PokedexView/PokedexView.js
@@ -1,22 +1,84 @@
-import React from 'react';
+import { useEffect, useState, React } from 'react';
+import { fetchPokemonData, fetchPokemons } from '../../api';
 import { Card } from '../Card';
+import { DetailsView } from '../DetailsView';
 import { Navigation } from '../Navigation';
+import { Loader } from '../Loader';
 import './PokedexView.css';
 
-function PokedexView({ pokemons, setSelectedPokemon })
+function PokedexView({ generation })
 {
+	const [ pokemons, setPokemons ] = useState( [] );
+	const [ selectedPokemon, setSelectedPokemon ] = useState( -1 );
+	const [ isLoading, setIsLoading ] = useState( false );
+
+	// load all pokemons on mount & generation change
+	useEffect( () => {
+
+		fillPokemonsArray();
+
+	}, [ generation ] );
+
+
+	// fetch all pokemons and push to array
+	const fillPokemonsArray = () => {
+
+		// set loading state and reset pokemons array
+		setIsLoading( true );
+		setPokemons( [] );
+
+		if( !generation.limit || !generation.offset )
+		{
+			return;
+		}
+
+		// fetch first original 151 pokemons
+		fetchPokemons( generation.limit, generation.offset ).then( async ({ results }) => {
+	
+			let newPokemons = []
+			
+			// iterate over each pokemon an add to array
+			await Promise.all( results.map( async ( pokemon, i ) => {
+
+				await fetchPokemonData( pokemon.name ).then( async ( json ) => {
+
+					newPokemons[i] = json;
+
+				});
+				
+			}));
+			
+			setPokemons( newPokemons );
+			setIsLoading( false );
+	
+		});
+		
+	}
+
+
+	if( isLoading )
+	{
+		return <Loader />;
+	}
+
+
     return (
 		<>
 			<Navigation />
+			
 			<div className="pokedex-view">
 				{
-					pokemons.map( p => {
+					pokemons.map( ( p, i ) => {
 						return (
-							<Card pokemon={ p } key={ p.id } setSelectedPokemon={ setSelectedPokemon } />
+							<Card pokemon={ p } key={ p.id } onClick={ () => setSelectedPokemon( i ) } />
 						);
 					})
 				}
 			</div>
+
+			{ selectedPokemon !== -1 &&
+				<DetailsView pokemon={ pokemons[selectedPokemon] } setSelectedPokemon={ setSelectedPokemon } />
+			}
 		</>
     )
 }

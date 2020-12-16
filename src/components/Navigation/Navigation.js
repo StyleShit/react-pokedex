540afc94983d1529663a762edae09bb2a75cfdc4
Evyatar Daud <evyatar.daud@gmail.com>
2020-12-16 12:00:45 +0200

    Move generations array to external file

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,69 +1,10 @@
 import React from 'react';
 import CustomNavLink from './CustomNavLink';
+import generations from '../../generations';
 import './Navigation.css';
 
 function Navigation()
 {
-    const generations = [
-        
-        {
-            link: 'gen-i',
-            text: 'I',
-            offset: 0,
-            limit: 151
-        },
-        
-        {
-            link: 'gen-ii',
-            text: 'II',
-            offset: 151,
-            limit: 100
-        },
-
-        {
-            link: 'gen-iii',
-            text: 'III',
-            offset: 251,
-            limit: 135
-        },
-
-        {
-            link: 'gen-iv',
-            text: 'IV',
-            offset: 386,
-            limit: 107
-        },
-
-        {
-            link: 'gen-v',
-            text: 'V',
-            offset: 493,
-            limit: 156
-        },
-
-        {
-            link: 'gen-vi',
-            text: 'VI',
-            offset: 649,
-            limit: 72
-        },
-
-        {
-            link: 'gen-vii',
-            text: 'VII',
-            offset: 721,
-            limit: 88
-        },
-
-        {
-            link: 'gen-viii',
-            text: 'VIII',
-            offset: 809,
-            limit: 89
-        },
-
-    ];
-
     return (
         <div className="navigation-container">
             <h3 className="navigation-title">Select Gen:</h3>
---
+++ src/generations.js
@@ -0,0 +1,61 @@
+const generations = [
+        
+    {
+        link: 'gen-i',
+        text: 'I',
+        offset: 0,
+        limit: 151
+    },
+    
+    {
+        link: 'gen-ii',
+        text: 'II',
+        offset: 151,
+        limit: 100
+    },
+
+    {
+        link: 'gen-iii',
+        text: 'III',
+        offset: 251,
+        limit: 135
+    },
+
+    {
+        link: 'gen-iv',
+        text: 'IV',
+        offset: 386,
+        limit: 107
+    },
+
+    {
+        link: 'gen-v',
+        text: 'V',
+        offset: 493,
+        limit: 156
+    },
+
+    {
+        link: 'gen-vi',
+        text: 'VI',
+        offset: 649,
+        limit: 72
+    },
+
+    {
+        link: 'gen-vii',
+        text: 'VII',
+        offset: 721,
+        limit: 88
+    },
+
+    {
+        link: 'gen-viii',
+        text: 'VIII',
+        offset: 809,
+        limit: 89
+    },
+
+];
+
+export default generations;

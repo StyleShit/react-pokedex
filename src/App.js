2e4a720973a54623a0cf58a9b5f00562a2a83a2b
Evyatar Daud <evyatar.daud@gmail.com>
2020-12-16 12:25:22 +0200

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { PokedexView } from './components/PokedexView';
 import { withRouter } from './HOCs';
 import generations from './generations';
@@ -5,7 +6,6 @@ import './App.css';
 
 function App()
 {
-
 	return (
 		<div className="pokedex-app">
 
@@ -13,7 +13,23 @@ function App()
 				P<i></i>kédex
 			</h1>
 
-			<PokedexView generation={ generations[0] } />
+			<Switch>
+
+				<Route exact path='/'>
+					<Redirect to={ generations[0].link } />
+				</Route>
+
+				{
+					generations.map( ( gen, i ) => (
+
+						<Route key={ i } exact path={ '/' + gen.link }>
+							<PokedexView generation={ gen } />
+						</Route>
+
+					))
+				}
+
+			</Switch>
 
 		</div>
 	);
